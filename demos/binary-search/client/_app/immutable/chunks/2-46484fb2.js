cc02d523c80394905d3ff3b818e81cf603d4054a
Geoff Cox <geoff.cox@live.com>
2022-12-27 14:02:46 -0800

    Published again

--- demos/binary-search/client/_app/immutable/chunks/2-46484fb2.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-b7b5277b.js";export{t as component};
--- demos/binary-search/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const file = '_app/immutable/components/error.svelte-b366d11f.js';
-export const imports = ["_app/immutable/components/error.svelte-b366d11f.js","_app/immutable/chunks/index-b39776cc.js","_app/immutable/chunks/singletons-8e60f9f9.js"];
-export const stylesheets = [];
-export const fonts = [];
