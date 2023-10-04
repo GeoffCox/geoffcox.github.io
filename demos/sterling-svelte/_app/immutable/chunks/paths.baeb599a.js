e8673a57334014b4e58d4cdac39ec89ddb032ccf
Geoff Cox <geoff.cox@live.com>
2023-10-03 17:41:40 -0700

    Published sterling-svelte doc for 1.0.1

---
+++ demos/sterling-svelte/_app/immutable/chunks/paths.baeb599a.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_h7flum)==null?void 0:s.base)??"/demos/sterling-svelte";var e;const l=((e=globalThis.__sveltekit_h7flum)==null?void 0:e.assets)??t;export{l as a,t as b};
