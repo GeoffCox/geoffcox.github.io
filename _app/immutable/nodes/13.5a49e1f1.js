865abf3dc290931d7621562c9500666ef72917f0
Geoff Cox <geoff.cox@live.com>
2023-10-29 13:07:15 -0700

    Update published

---
+++ _app/immutable/nodes/13.5a49e1f1.js
@@ -0,0 +1,13 @@
+import{s as r,n as o}from"../chunks/scheduler.7a81b44c.js";import{S as s,i as d,e as l,c as h,F as c,o as p,j as u,g as m}from"../chunks/index.43019d97.js";function y(i){let e,n=`<div class="category svelte-wvk4c8">Storytime</div> <div class="headline svelte-wvk4c8">Users are unpredictable</div> <p>Back in the days of Windows 95, my brother called me in a panic. He reported that his drive
+        was out of space and he couldn&#39;t figure out what to do.</p> <p>I walked him through selecting some files he didn&#39;t need anymore. When I told him to drag the
+        files to the recycle bin in Windows Explorer, he couldn&#39;t find it. I asked him to look on his
+        desktop and he reported it wasn&#39;t there either.</p> <p>He then told me what he had done the day before while trying to clean up files on his desktop.
+        He had somehow dragged the recycle bin icon around and ended up creating a shortcut to the
+        recycle bin. He now had two recycle bins on his desktop. He dragged one on to to the other and
+        then emptied the recycle bin.</p> <p>Turns out he dragged the real recycle bin to the recycle bin shortcut. This put the recycle
+        bin control panel app into the recycle bin. When he emptied the recycle bin, the control panel
+        app deleted itself!</p> <p>The OS went through the recycle bin app for soft and hard deletes. Ctrl+Del didn&#39;t do
+        anything. We were able to open a command prompt and delete files, but this was too tedious for
+        him to do regularly without my help. He just kind of lived with a too full drive until he
+        bought his next computer.</p> <p>I doubt any developer or tester could have predicted a user would use a shortcut to the
+        recycle bin to delete the recycle bin.</p>`;return{c(){e=l("div"),e.innerHTML=n,this.h()},l(t){e=h(t,"DIV",{class:!0,"data-svelte-h":!0}),c(e)!=="svelte-pxup70"&&(e.innerHTML=n),this.h()},h(){p(e,"class","blurb")},m(t,a){u(t,e,a)},p:o,i:o,o,d(t){t&&m(e)}}}class f extends s{constructor(e){super(),d(this,e,null,y,r,{})}}export{f as component};
---
+++ _app/immutable/nodes/14.ad30b53f.js
@@ -0,0 +1,13 @@
+import{s as o,n as a}from"../chunks/scheduler.7a81b44c.js";import{S as l,i as r,e as d,c,F as p,o as v,j as h,g as m}from"../chunks/index.43019d97.js";function u(n){let e,i=`<div class="category svelte-1nswsbw">Opinion</div> <div class="headline svelte-1nswsbw">Why I love Svelte (and SvelteKit)</div> <div class="details">If you&#39;ve never heard of them, check out <a href="https://svelte.dev/" target="_blank">svelte.dev</a>
+		and
+		<a href="https://kit.svelte.dev/" target="_blank">kit.svelte.dev</a>.
+		<p>I am more productive. Writing HTML, CSS, and JS is straigtforward. Svelte&#39;s hot-module-reloading
+			makes my inner dev loop very fast. I feel like I can code anything with Svelte.</p> <p>I can properly architect and design. Svelte provides componentization, encapsulation, composition,
+			and extensibility.</p> <p>I don&#39;t have to make as many low-level decisions. I don&#39;t agonize over which state management,
+			templating, or styling libraries to choose. Svelte meets all my needs for building applications
+			and libraries.</p> <p>I don&#39;t waste time trying to optimize my code by hand. There&#39;s no need call optimization
+			memo functions since the Svelte compiler makes the code as fast and small.</p> <p>I quickly became an expert. Svelte&#39;s documentation is concise and precise. The tutorial
+			and examples get you started in a couple of hours. Addtional capabilities like SvelteKit are built
+			on top of rather than inside Svelte. This helps Svelte avoid collapsing under the weight of additional
+			complexity.</p> <p>Even if you are a React or Angular fanatic, I hope you&#39;ll check out Svelte. I&#39;ve found that
+			learning new languages and technologies helps me be better with the ones I know.</p></div>`;return{c(){e=d("div"),e.innerHTML=i,this.h()},l(t){e=c(t,"DIV",{class:!0,"data-svelte-h":!0}),p(e)!=="svelte-i097em"&&(e.innerHTML=i),this.h()},h(){v(e,"class","root")},m(t,s){h(t,e,s)},p:a,i:a,o:a,d(t){t&&m(e)}}}class y extends l{constructor(e){super(),r(this,e,null,u,o,{})}}export{y as component};
