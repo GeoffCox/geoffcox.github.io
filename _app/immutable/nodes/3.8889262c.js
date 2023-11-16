90b1ad40385da1c72579ea528310eff6c9c2f49a
Geoff Cox <geoff.cox@live.com>
2023-11-15 18:37:10 -0800

    Publish glog

---
+++ _app/immutable/nodes/3.8889262c.js
@@ -0,0 +1,49 @@
+import{s as Vt,e as ht,n as wt,f as yt,h as Dt,c as jt,u as Ht,g as zt,a as Mt}from"../chunks/scheduler.bebf7e56.js";import{S as It,i as St,w as h,j as Bt,x as v,k as a,f as n,l as Rt,b as t,y as Ct,d as At,m as e,e as f,o as kt,s as w,c as u,p as Et,a as y,g as A,q as $t,t as it,h as ct,r as bt}from"../chunks/index.eec01950.js";import{H as Gt}from"../chunks/Header.75c83bf5.js";import{g as qt}from"../chunks/spread.84d39b6c.js";function Ot(H){let l,d,_,o,i,s,C,k,R,g,O,G,m,z,$,x,D,b,p,M,F=[{version:"1.1"},{xmlns:"http://www.w3.org/2000/svg"},{"xmlns:xlink":"http://www.w3.org/1999/xlink"},{viewBox:"0 0 500 500"},{"xml:space":"preserve"},{fill:"#000000"},{width:"60px"},{height:"60px"},H[0]],V={};for(let E=0;E<F.length;E+=1)V=ht(V,F[E]);return{c(){l=h("svg"),d=h("g"),_=h("g"),o=h("g"),i=h("style"),s=Bt(`.st0 {
+				fill: #ffffff;
+				stroke: #ffffff;
+				stroke-width: 10;
+				stroke-miterlimit: 10;
+			}
+			.st1 {
+				fill: #dbc491;
+			}
+			.st2 {
+				fill: #b92900;
+			}
+			.st3 {
+				fill: #785830;
+			}
+			.st4 {
+				fill: #ccfffc;
+			}
+			.st5 {
+				fill: #805f35;
+			}
+			.st6 {
+				fill: #d6ae5c;
+			}
+		`),C=h("g"),k=h("polygon"),R=h("g"),g=h("g"),O=h("polygon"),G=h("polygon"),m=h("path"),z=h("g"),$=h("rect"),x=h("rect"),D=h("rect"),b=h("rect"),p=h("rect"),M=h("circle"),this.h()},l(E){l=v(E,"svg",{version:!0,xmlns:!0,"xmlns:xlink":!0,viewBox:!0,"xml:space":!0,fill:!0,width:!0,height:!0});var j=a(l);d=v(j,"g",{id:!0,"stroke-width":!0}),a(d).forEach(n),_=v(j,"g",{id:!0,"stroke-linecap":!0,"stroke-linejoin":!0}),a(_).forEach(n),o=v(j,"g",{id:!0});var I=a(o);i=v(I,"style",{type:!0});var Z=a(i);s=Rt(Z,`.st0 {
+				fill: #ffffff;
+				stroke: #ffffff;
+				stroke-width: 10;
+				stroke-miterlimit: 10;
+			}
+			.st1 {
+				fill: #dbc491;
+			}
+			.st2 {
+				fill: #b92900;
+			}
+			.st3 {
+				fill: #785830;
+			}
+			.st4 {
+				fill: #ccfffc;
+			}
+			.st5 {
+				fill: #805f35;
+			}
+			.st6 {
+				fill: #d6ae5c;
+			}
+		`),Z.forEach(n),C=v(I,"g",{id:!0});var T=a(C);k=v(T,"polygon",{class:!0,points:!0}),a(k).forEach(n),T.forEach(n),R=v(I,"g",{id:!0});var B=a(R);g=v(B,"g",{});var q=a(g);O=v(q,"polygon",{class:!0,points:!0}),a(O).forEach(n),G=v(q,"polygon",{class:!0,points:!0}),a(G).forEach(n),m=v(q,"path",{class:!0,d:!0}),a(m).forEach(n),z=v(q,"g",{});var U=a(z);$=v(U,"rect",{x:!0,y:!0,class:!0,width:!0,height:!0}),a($).forEach(n),x=v(U,"rect",{x:!0,y:!0,class:!0,width:!0,height:!0}),a(x).forEach(n),D=v(U,"rect",{x:!0,y:!0,class:!0,width:!0,height:!0}),a(D).forEach(n),b=v(U,"rect",{x:!0,y:!0,class:!0,width:!0,height:!0}),a(b).forEach(n),U.forEach(n),p=v(q,"rect",{x:!0,y:!0,class:!0,width:!0,height:!0}),a(p).forEach(n),M=v(q,"circle",{class:!0,cx:!0,cy:!0,r:!0}),a(M).forEach(n),q.forEach(n),B.forEach(n),I.forEach(n),j.forEach(n),this.h()},h(){t(d,"id","SVGRepo_bgCarrier"),t(d,"stroke-width","0"),t(_,"id","SVGRepo_tracerCarrier"),t(_,"stroke-linecap","round"),t(_,"stroke-linejoin","round"),t(i,"type","text/css"),t(k,"class","st0"),t(k,"points","453.2,255.6 250,82.9 46.8,255.6 71.7,276.7 96.6,255.5 96.6,329.8 96.6,381 96.6,417.1 403.4,417.1 403.4,381 403.4,329.8 403.4,255.5 428.3,276.7 "),t(C,"id","border"),t(O,"class","st1"),t(O,"points","250,125.2 96.6,255.5 96.6,329.8 96.6,381 96.6,417.1 403.4,417.1 403.4,381 403.4,329.8 403.4,255.5 "),t(G,"class","st2"),t(G,"points","250,125.2 428.3,276.7 453.2,255.6 250,82.9 46.8,255.6 71.7,276.7 "),t(m,"class","st3"),t(m,"d","M279.8,268.7v73.6h92v-73.6H279.8z M359.4,301.3H330v-20h29.4V301.3z M321.5,281.3v20h-29.4v-20H321.5z M292.2,309.8h29.4v22.1h-29.4V309.8z M330,331.9v-22.1h29.4v22.1H330z"),t($,"x","330"),t($,"y","309.8"),t($,"class","st4"),t($,"width","29.4"),t($,"height","22.1"),t(x,"x","292.2"),t(x,"y","281.3"),t(x,"class","st4"),t(x,"width","29.4"),t(x,"height","20"),t(D,"x","292.2"),t(D,"y","309.8"),t(D,"class","st4"),t(D,"width","29.4"),t(D,"height","22.1"),t(b,"x","330"),t(b,"y","281.3"),t(b,"class","st4"),t(b,"width","29.4"),t(b,"height","20"),t(p,"x","140"),t(p,"y","248.6"),t(p,"class","st5"),t(p,"width","102.2"),t(p,"height","167"),t(M,"class","st6"),t(M,"cx","217.1"),t(M,"cy","326.5"),t(M,"r","9.2"),t(R,"id","object"),t(o,"id","SVGRepo_iconCarrier"),Ct(l,V)},m(E,j){At(E,l,j),e(l,d),e(l,_),e(l,o),e(o,i),e(i,s),e(o,C),e(C,k),e(o,R),e(R,g),e(g,O),e(g,G),e(g,m),e(g,z),e(z,$),e(z,x),e(z,D),e(z,b),e(g,p),e(g,M)},p(E,[j]){Ct(l,V=qt(F,[{version:"1.1"},{xmlns:"http://www.w3.org/2000/svg"},{"xmlns:xlink":"http://www.w3.org/1999/xlink"},{viewBox:"0 0 500 500"},{"xml:space":"preserve"},{fill:"#000000"},{width:"60px"},{height:"60px"},j&1&&E[0]]))},i:wt,o:wt,d(E){E&&n(l)}}}function Ut(H,l,d){const _=[];let o=yt(l,_);return H.$$set=i=>{l=ht(ht({},l),Dt(i)),d(0,o=yt(l,_))},[o]}class Kt extends It{constructor(l){super(),St(this,l,Ut,Ot,Vt,{})}}function Lt(H){let l,d,_,o,i,s,C,k,R,g,O="Recent",G,m,z="I shipped a UI component library for Svelte!",$,x,D="Open Source",b,p,M="binary-search",F,V,E="pretty-good-nlp",j,I,Z="recoil-examples",T,B,q="splitters",U,J,vt="Opinion",et,L,ft="Why I love Svelte (and SvelteKit)",st,N,ut="Stories",lt,P,dt="Unpredictable users",rt,Q,gt="Bookshelf",nt,W,pt="Software Architecture",at,X,Y;d=new Gt({}),k=new Kt({props:{width:"42px",height:"42px"}});const ot=H[1].default,S=jt(ot,H,H[0],null);return{c(){l=f("div"),kt(d.$$.fragment),_=w(),o=f("div"),i=f("div"),s=f("div"),C=f("a"),kt(k.$$.fragment),R=w(),g=f("div"),g.textContent=O,G=w(),m=f("a"),m.textContent=z,$=w(),x=f("div"),x.textContent=D,b=w(),p=f("a"),p.textContent=M,F=w(),V=f("a"),V.textContent=E,j=w(),I=f("a"),I.textContent=Z,T=w(),B=f("a"),B.textContent=q,U=w(),J=f("div"),J.textContent=vt,et=w(),L=f("a"),L.textContent=ft,st=w(),N=f("div"),N.textContent=ut,lt=w(),P=f("a"),P.textContent=dt,rt=w(),Q=f("div"),Q.textContent=gt,nt=w(),W=f("a"),W.textContent=pt,at=w(),X=f("div"),S&&S.c(),this.h()},l(c){l=u(c,"DIV",{class:!0});var K=a(l);Et(d.$$.fragment,K),_=y(K),o=u(K,"DIV",{class:!0});var xt=a(o);i=u(xt,"DIV",{class:!0});var tt=a(i);s=u(tt,"DIV",{class:!0});var r=a(s);C=u(r,"A",{href:!0,class:!0});var _t=a(C);Et(k.$$.fragment,_t),_t.forEach(n),R=y(r),g=u(r,"DIV",{class:!0,"data-svelte-h":!0}),A(g)!=="svelte-6j1nnp"&&(g.textContent=O),G=y(r),m=u(r,"A",{href:!0,class:!0,"data-svelte-h":!0}),A(m)!=="svelte-1wt11oj"&&(m.textContent=z),$=y(r),x=u(r,"DIV",{class:!0,"data-svelte-h":!0}),A(x)!=="svelte-bb8go7"&&(x.textContent=D),b=y(r),p=u(r,"A",{href:!0,class:!0,"data-svelte-h":!0}),A(p)!=="svelte-18fiwxn"&&(p.textContent=M),F=y(r),V=u(r,"A",{href:!0,class:!0,"data-svelte-h":!0}),A(V)!=="svelte-z547o9"&&(V.textContent=E),j=y(r),I=u(r,"A",{href:!0,class:!0,"data-svelte-h":!0}),A(I)!=="svelte-1a5kdgr"&&(I.textContent=Z),T=y(r),B=u(r,"A",{href:!0,class:!0,"data-svelte-h":!0}),A(B)!=="svelte-1jwtgnb"&&(B.textContent=q),U=y(r),J=u(r,"DIV",{class:!0,"data-svelte-h":!0}),A(J)!=="svelte-1hf63oi"&&(J.textContent=vt),et=y(r),L=u(r,"A",{href:!0,class:!0,"data-svelte-h":!0}),A(L)!=="svelte-117tknh"&&(L.textContent=ft),st=y(r),N=u(r,"DIV",{class:!0,"data-svelte-h":!0}),A(N)!=="svelte-1etjljf"&&(N.textContent=ut),lt=y(r),P=u(r,"A",{href:!0,class:!0,"data-svelte-h":!0}),A(P)!=="svelte-3gwilo"&&(P.textContent=dt),rt=y(r),Q=u(r,"DIV",{class:!0,"data-svelte-h":!0}),A(Q)!=="svelte-1sf3y4h"&&(Q.textContent=gt),nt=y(r),W=u(r,"A",{href:!0,class:!0,"data-svelte-h":!0}),A(W)!=="svelte-fkcxab"&&(W.textContent=pt),r.forEach(n),at=y(tt),X=u(tt,"DIV",{class:!0});var mt=a(X);S&&S.l(mt),mt.forEach(n),tt.forEach(n),xt.forEach(n),K.forEach(n),this.h()},h(){t(C,"href","/"),t(C,"class","home-link svelte-hvv8kn"),t(g,"class","nav-header svelte-hvv8kn"),t(m,"href","/sterling-svelte"),t(m,"class","svelte-hvv8kn"),t(x,"class","nav-header svelte-hvv8kn"),t(p,"href","/binary-search"),t(p,"class","svelte-hvv8kn"),t(V,"href","/pretty-good-nlp"),t(V,"class","svelte-hvv8kn"),t(I,"href","/recoil-examples"),t(I,"class","svelte-hvv8kn"),t(B,"href","/splitters"),t(B,"class","svelte-hvv8kn"),t(J,"class","nav-header svelte-hvv8kn"),t(L,"href","/why-i-love-svelte"),t(L,"class","svelte-hvv8kn"),t(N,"class","nav-header svelte-hvv8kn"),t(P,"href","/unpredictable-users"),t(P,"class","svelte-hvv8kn"),t(Q,"class","nav-header svelte-hvv8kn"),t(W,"href","/software-architecture-books"),t(W,"class","svelte-hvv8kn"),t(s,"class","nav svelte-hvv8kn"),t(X,"class","content svelte-hvv8kn"),t(i,"class","split svelte-hvv8kn"),t(o,"class","centered svelte-hvv8kn"),t(l,"class","root")},m(c,K){At(c,l,K),$t(d,l,null),e(l,_),e(l,o),e(o,i),e(i,s),e(s,C),$t(k,C,null),e(s,R),e(s,g),e(s,G),e(s,m),e(s,$),e(s,x),e(s,b),e(s,p),e(s,F),e(s,V),e(s,j),e(s,I),e(s,T),e(s,B),e(s,U),e(s,J),e(s,et),e(s,L),e(s,st),e(s,N),e(s,lt),e(s,P),e(s,rt),e(s,Q),e(s,nt),e(s,W),e(i,at),e(i,X),S&&S.m(X,null),Y=!0},p(c,[K]){S&&S.p&&(!Y||K&1)&&Ht(S,ot,c,c[0],Y?Mt(ot,c[0],K,null):zt(c[0]),null)},i(c){Y||(it(d.$$.fragment,c),it(k.$$.fragment,c),it(S,c),Y=!0)},o(c){ct(d.$$.fragment,c),ct(k.$$.fragment,c),ct(S,c),Y=!1},d(c){c&&n(l),bt(d),bt(k),S&&S.d(c)}}}function Pt(H,l,d){let{$$slots:_={},$$scope:o}=l;return H.$$set=i=>{"$$scope"in i&&d(0,o=i.$$scope)},[o,_]}class Qt extends It{constructor(l){super(),St(this,l,Pt,Lt,Vt,{})}}export{Qt as component};
