400062a128ead885af9c2db41f11f591c5d9ea9c
Geoff Cox <geoff.cox@live.com>
2024-06-04 10:38:29 -0700

    Published sterling-svelte 1.0.6

---
+++ demos/sterling-svelte/_app/immutable/chunks/SvelteIcon.DockpRgu.js
@@ -0,0 +1,22 @@
+import{s as B,o as x,C as f,t as y,D as p,d as m,f as k,h as i,j as v,E as C,k as L,l as o,n as E,y as S,z as M}from"./scheduler.Bw_xjNFK.js";import{g as b}from"./spread.CgU5AtxT.js";import{S as j,i as q}from"./index.BMoFg06t.js";function z(_){let t,n,c,s,h,a,r,u=[{version:"1.1"},{xmlns:"http://www.w3.org/2000/svg"},{"xmlns:xlink":"http://www.w3.org/1999/xlink"},{viewBox:"0 0 98.1 118"},{width:"1.5em"},{height:"1.5em"},{"xml:space":"preserve"},_[0]],g={};for(let e=0;e<u.length;e+=1)g=x(g,u[e]);return{c(){t=f("svg"),n=f("title"),c=y("Svelte"),s=f("style"),h=y(`.st0 {
+      fill: #ff3e00;
+    }
+    .st1 {
+      fill: #ffffff;
+    }
+  `),a=f("path"),r=f("path"),this.h()},l(e){t=p(e,"svg",{version:!0,xmlns:!0,"xmlns:xlink":!0,viewBox:!0,width:!0,height:!0,"xml:space":!0});var l=m(t);n=p(l,"title",{});var d=m(n);c=k(d,"Svelte"),d.forEach(i),s=p(l,"style",{type:!0});var w=m(s);h=k(w,`.st0 {
+      fill: #ff3e00;
+    }
+    .st1 {
+      fill: #ffffff;
+    }
+  `),w.forEach(i),a=p(l,"path",{class:!0,d:!0}),m(a).forEach(i),r=p(l,"path",{class:!0,d:!0}),m(r).forEach(i),l.forEach(i),this.h()},h(){v(s,"type","text/css"),v(a,"class","st0"),v(a,"d",`M91.8,15.6C80.9-0.1,59.2-4.7,43.6,5.2L16.1,22.8C8.6,27.5,3.4,35.2,1.9,43.9c-1.3,7.3-0.2,14.8,3.3,21.3
+	c-2.4,3.6-4,7.6-4.7,11.8c-1.6,8.9,0.5,18.1,5.7,25.4c11,15.7,32.6,20.3,48.2,10.4l27.5-17.5c7.5-4.7,12.7-12.4,14.2-21.1
+	c1.3-7.3,0.2-14.8-3.3-21.3c2.4-3.6,4-7.6,4.7-11.8C99.2,32.1,97.1,22.9,91.8,15.6`),v(r,"class","st1"),v(r,"d",`M40.9,103.9c-8.9,2.3-18.2-1.2-23.4-8.7c-3.2-4.4-4.4-9.9-3.5-15.3c0.2-0.9,0.4-1.7,0.6-2.6l0.5-1.6l1.4,1
+	c3.3,2.4,6.9,4.2,10.8,5.4l1,0.3l-0.1,1c-0.1,1.4,0.3,2.9,1.1,4.1c1.6,2.3,4.4,3.4,7.1,2.7c0.6-0.2,1.2-0.4,1.7-0.7L65.5,72
+	c1.4-0.9,2.3-2.2,2.6-3.8c0.3-1.6-0.1-3.3-1-4.6c-1.6-2.3-4.4-3.3-7.1-2.6c-0.6,0.2-1.2,0.4-1.7,0.7l-10.5,6.7
+	c-1.7,1.1-3.6,1.9-5.6,2.4c-8.9,2.3-18.2-1.2-23.4-8.7c-3.1-4.4-4.4-9.9-3.4-15.3c0.9-5.2,4.1-9.9,8.6-12.7l27.5-17.5
+	c1.7-1.1,3.6-1.9,5.6-2.5c8.9-2.3,18.2,1.2,23.4,8.7c3.2,4.4,4.4,9.9,3.5,15.3c-0.2,0.9-0.4,1.7-0.7,2.6l-0.5,1.6l-1.4-1
+	c-3.3-2.4-6.9-4.2-10.8-5.4l-1-0.3l0.1-1c0.1-1.4-0.3-2.9-1.1-4.1c-1.6-2.3-4.4-3.3-7.1-2.6c-0.6,0.2-1.2,0.4-1.7,0.7L32.4,46.1
+	c-1.4,0.9-2.3,2.2-2.6,3.8s0.1,3.3,1,4.6c1.6,2.3,4.4,3.3,7.1,2.6c0.6-0.2,1.2-0.4,1.7-0.7l10.5-6.7c1.7-1.1,3.6-1.9,5.6-2.5
+	c8.9-2.3,18.2,1.2,23.4,8.7c3.2,4.4,4.4,9.9,3.5,15.3c-0.9,5.2-4.1,9.9-8.6,12.7l-27.5,17.5C44.8,102.5,42.9,103.3,40.9,103.9`),C(t,g)},m(e,l){L(e,t,l),o(t,n),o(n,c),o(t,s),o(s,h),o(t,a),o(t,r)},p(e,[l]){C(t,g=b(u,[{version:"1.1"},{xmlns:"http://www.w3.org/2000/svg"},{"xmlns:xlink":"http://www.w3.org/1999/xlink"},{viewBox:"0 0 98.1 118"},{width:"1.5em"},{height:"1.5em"},{"xml:space":"preserve"},l&1&&e[0]]))},i:E,o:E,d(e){e&&i(t)}}}function D(_,t,n){const c=[];let s=S(t,c);return _.$$set=h=>{t=x(x({},t),M(h)),n(0,s=S(t,c))},[s]}class F extends j{constructor(t){super(),q(this,t,D,z,B,{})}}export{F as S};
