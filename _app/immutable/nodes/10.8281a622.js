f23e8d4701287dee5777920746611960b68821a4
Geoff Cox <geoff.cox@live.com>
2025-01-23 15:25:57 -0800

    Updated for resume post

---
+++ _app/immutable/nodes/10.8281a622.js
@@ -0,0 +1,28 @@
+import{s as M,a as A,k as F,m as N,e as h,p as c,b as u,q as p,t as f,i as o,w as O,f as s}from"../chunks/scheduler.75f5293f.js";import{S as R,i as U,c as B,b as G,m as J,t as K,a as V,d as X}from"../chunks/index.36074062.js";import{g as Y,a as Z}from"../chunks/spread.8a54911c.js";import{P as tt}from"../chunks/Post.19bcd7ab.js";function et(C){let e,m=`One evening, when I was working on a large customer data store, there was an emergency team
+		meeting to figure out how to handle a bug that had incorrectly hashed the Passport
+		authentication identifiers in our SQL databases. About 20 of us were called into a 10 person
+		conference room to discuss options.`,i,l,n=`A dev on my team reported he had already patched the code and had a SQL script to repair the
+		existing data. However, some random guy from another team had been talking to my boss and
+		followed us into the meeting room. He decided to take over the meeting and loudly declared that
+		we first had to estimate how long running the script would take. My boss, normally outspoken and
+		decisive, deferred to him. Everyone assumed he must be important and knew what he was doing.`,r,d,W=`He grabbed a whiteboard pen and starting doing a calculation on the board. He stated that
+		updating a row in SQL probably takes at best 1 millisecond when within a transaction. He also
+		wrote out a bunch of hash algorithm statements that he said added up to 2.5 milliseconds per
+		row. We had 400 million rows that needed updating. He then calculated this would take 400
+		million x 3.5 = 1.4 billion milliseconds => 1.4 million seconds => 23 thousand minutes
+		=> 388 hours => 16 days.`,k,w,j=`No one could use the system until all the rows were updated. The room was in a panic. I thought
+		that we should get the script started ASAP. I pulled the dev and a service engineer into the
+		hallway to confirm they had confidence in the script and to ask them to get it running.`,P,g,q=`In the meantime, random guy started drawing up a shift rotation schedule for each person to be
+		on site watching the script run over the next 16 days. After 15 minutes, as random guy was
+		finalizing the shift schedule, the dev and service engineer returned. The dev whispered in my
+		ear the results.`,$,v,z=`Random guy was literally giving a pep talk to the room about how hard it was going to be working
+		24 hours a day, but that it would be a worthy and valiant effort. I tried to interrupt him, but
+		he didn't give me any opportunity.`,I,y,L=`Finally, he finished. I let the room know that we had run the script and it completed in under
+		15 minutes. The script updated over 400 rows per millisecond. He muttered that was impossible as
+		his chest caved and he hung his head.`,S,_,Q=`While this guy was a bit of a blow hard, he followed how developers are taught to estimate
+		algorithms. We mentally walk through the steps, estimate the cost of each instruction, account
+		for loops, and multiply by the number of items to get the result.`,H,b,E=`The problem was that computation speed had exceeded human imagination. It is hard for humans to
+		understand how fast 1/400th of a millisecond goes by. We like to think we can understand that
+		kind of processing power, but we can't be precise enough to avoid exponential errors. This was
+		back in 2005. Today, that script would likely complete in seconds.`,T,x,D=`We shouldn't manually think through estimates. Instead, we have to run the code in a
+		production/test environment and measure the real performance. Don't be that (random) guy.`;return{c(){e=h("p"),e.textContent=m,i=c(),l=h("p"),l.textContent=n,r=c(),d=h("p"),d.textContent=W,k=c(),w=h("p"),w.textContent=j,P=c(),g=h("p"),g.textContent=q,$=c(),v=h("p"),v.textContent=z,I=c(),y=h("p"),y.textContent=L,S=c(),_=h("p"),_.textContent=Q,H=c(),b=h("p"),b.textContent=E,T=c(),x=h("p"),x.textContent=D},l(t){e=u(t,"P",{"data-svelte-h":!0}),p(e)!=="svelte-zi2g9c"&&(e.textContent=m),i=f(t),l=u(t,"P",{"data-svelte-h":!0}),p(l)!=="svelte-139gv2o"&&(l.textContent=n),r=f(t),d=u(t,"P",{"data-svelte-h":!0}),p(d)!=="svelte-1qkpabd"&&(d.textContent=W),k=f(t),w=u(t,"P",{"data-svelte-h":!0}),p(w)!=="svelte-11ih7t0"&&(w.textContent=j),P=f(t),g=u(t,"P",{"data-svelte-h":!0}),p(g)!=="svelte-o6jwum"&&(g.textContent=q),$=f(t),v=u(t,"P",{"data-svelte-h":!0}),p(v)!=="svelte-9eoe3a"&&(v.textContent=z),I=f(t),y=u(t,"P",{"data-svelte-h":!0}),p(y)!=="svelte-1j8jwfq"&&(y.textContent=L),S=f(t),_=u(t,"P",{"data-svelte-h":!0}),p(_)!=="svelte-bcazty"&&(_.textContent=Q),H=f(t),b=u(t,"P",{"data-svelte-h":!0}),p(b)!=="svelte-a3ynid"&&(b.textContent=E),T=f(t),x=u(t,"P",{"data-svelte-h":!0}),p(x)!=="svelte-1prs90w"&&(x.textContent=D)},m(t,a){o(t,e,a),o(t,i,a),o(t,l,a),o(t,r,a),o(t,d,a),o(t,k,a),o(t,w,a),o(t,P,a),o(t,g,a),o(t,$,a),o(t,v,a),o(t,I,a),o(t,y,a),o(t,S,a),o(t,_,a),o(t,H,a),o(t,b,a),o(t,T,a),o(t,x,a)},p:O,d(t){t&&(s(e),s(i),s(l),s(r),s(d),s(k),s(w),s(P),s(g),s($),s(v),s(I),s(y),s(S),s(_),s(H),s(b),s(T),s(x))}}}function nt(C){let e,m;const i=[{category:"Story"},{headline:"Speed Estimation"},{postUrl:"speed-estimation"},C[0]];let l={$$slots:{default:[et]},$$scope:{ctx:C}};for(let n=0;n<i.length;n+=1)l=A(l,i[n]);return e=new tt({props:l}),{c(){B(e.$$.fragment)},l(n){G(e.$$.fragment,n)},m(n,r){J(e,n,r),m=!0},p(n,[r]){const d=r&1?Y(i,[i[0],i[1],i[2],Z(n[0])]):{};r&2&&(d.$$scope={dirty:r,ctx:n}),e.$set(d)},i(n){m||(K(e.$$.fragment,n),m=!0)},o(n){V(e.$$.fragment,n),m=!1},d(n){X(e,n)}}}function at(C,e,m){const i=[];let l=F(e,i);return C.$$set=n=>{e=A(A({},e),N(n)),m(0,l=F(e,i))},[l]}class rt extends R{constructor(e){super(),U(this,e,at,nt,M,{})}}export{rt as component};
---
+++ _app/immutable/nodes/19.bb1a06e9.js
@@ -0,0 +1,9 @@
+import{s as B,x as f,e as c,y as u,b as $,i as l,w as R,f as i}from"../chunks/scheduler.75f5293f.js";import{S as E,i as I,c as y,b as x,m as d,t as g,a as k,d as v}from"../chunks/index.36074062.js";import{P as q}from"../chunks/Post.19bcd7ab.js";import{Q as P}from"../chunks/Quote.1a10aae0.js";function S(m){let t,o,e,s,r,p,_,w,b,h;return{c(){t=f("Everyone is afraid of losing"),o=c("br"),e=f(`
+Even the ones that always win`),s=c("br"),r=f(`
+Hey sleepwalker, when the mountain comes back to life`),p=c("br"),_=f(`
+It doesn't come from without`),w=c("br"),b=f(`
+It comes from within`),h=c("br")},l(n){t=u(n,"Everyone is afraid of losing"),o=$(n,"BR",{}),e=u(n,`
+Even the ones that always win`),s=$(n,"BR",{}),r=u(n,`
+Hey sleepwalker, when the mountain comes back to life`),p=$(n,"BR",{}),_=u(n,`
+It doesn't come from without`),w=$(n,"BR",{}),b=u(n,`
+It comes from within`),h=$(n,"BR",{})},m(n,a){l(n,t,a),l(n,o,a),l(n,e,a),l(n,s,a),l(n,r,a),l(n,p,a),l(n,_,a),l(n,w,a),l(n,b,a),l(n,h,a)},p:R,d(n){n&&(i(t),i(o),i(e),i(s),i(r),i(p),i(_),i(w),i(b),i(h))}}}function H(m){let t,o;return t=new P({props:{source:"Sleepwalker - The Killers",$$slots:{default:[S]},$$scope:{ctx:m}}}),{c(){y(t.$$.fragment)},l(e){x(t.$$.fragment,e)},m(e,s){d(t,e,s),o=!0},p(e,s){const r={};s&1&&(r.$$scope={dirty:s,ctx:e}),t.$set(r)},i(e){o||(g(t.$$.fragment,e),o=!0)},o(e){k(t.$$.fragment,e),o=!1},d(e){v(t,e)}}}function Q(m){let t,o;return t=new q({props:{$$slots:{default:[H]},$$scope:{ctx:m}}}),{c(){y(t.$$.fragment)},l(e){x(t.$$.fragment,e)},m(e,s){d(t,e,s),o=!0},p(e,[s]){const r={};s&1&&(r.$$scope={dirty:s,ctx:e}),t.$set(r)},i(e){o||(g(t.$$.fragment,e),o=!0)},o(e){k(t.$$.fragment,e),o=!1},d(e){v(t,e)}}}class z extends E{constructor(t){super(),I(this,t,null,Q,B,{})}}export{z as component};
